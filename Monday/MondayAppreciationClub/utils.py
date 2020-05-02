8c0d0882b21745b81fd59b8eff004b2e34bb599a
Rajat Kumar <rajatkumar0410@gmail.com>
2020-05-02 19:37:34 -0400

    Implemented the look_up function to return a dictionary containing valuable information regarding the stock that is being looked up

--- Monday/MondayAppreciationClub/utils.py
+++ Monday/MondayAppreciationClub/utils.py
@@ -1,10 +1,22 @@
 import yfinance as yf
 import pandas as pd
 
-def look_up(stock):
-    data = yf.Ticker(stock)
-    print(data.info)
-    hist = data.history(period="5d")
-    print(hist)
+def look_up(stock,total_time = "5d",interval_time = "1d"):
 
-look_up("WEF.TO")
+    data = yf.Ticker(stock)
+
+    original = data.info
+    #White list picks the values we want to KEEP from the original data.info
+    white_list = {"sector", "fullTimeEmployees", "longBusinessSummary", "city", "phone",
+                  "state", "country", "website", "address1", "industry",
+                  "currency", "marketCap", "sharesOutstanding", "bookValue", "logo_url"}
+
+    returnDict = {k:v for k,v in original.items() if k in white_list}
+
+    #Creates a dictionary that contains numerical information like open/close/volume for specific dates of the time
+    hist = data.history(period=total_time,interval = interval_time).to_dict()
+
+    #Combines both dictionaries into one
+    returnDict.update(hist)
+
+    return returnDict
