f502d2abb95236ee8316746954ee55966b7809ce
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-05-03 05:46:44 -0400

    cahnges

--- Monday/MondayAppreciationClub/models.py
+++ Monday/MondayAppreciationClub/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Stock(models.Model):
-    ticker = models.CharField(max_length=5)
-    company_name = models.TextField()
+    ticker = models.CharField(max_length=5, default="_")
+    company_name = models.TextField(default="_")
 
--- Monday/MondayAppreciationClub/urls.py
+++ Monday/MondayAppreciationClub/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.ticker, name="ticker"),
     path('newsinfo/', views.news_info, name='newsinfo'),
     path('stockinfo/', views.lookup_info, name='stockinfo'),
 ]
