9e7aab0bb630b46fefbd9623f1cd10e221b332eb
Cashaman <61897338+Cashaman@users.noreply.github.com>
2020-05-03 11:14:26 -0400

    about page filled

--- aWebApp/src/app/app-routing.module.ts
+++ aWebApp/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SingleStockComponent } from './single-stock/single-stock.component';
 import { StocksComponent } from './stocks/stocks.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'singlestock', component: SingleStockComponent },
   { path: 'stock', component: StocksComponent },
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
