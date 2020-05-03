2e9f86fcbf4f615522e1cc6676273028addd4d4b
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-05-03 09:33:14 -0400

    push

--- aWebApp/src/app/stocks/stocks.component.ts
+++ aWebApp/src/app/stocks/stocks.component.ts
@@ -11,13 +11,21 @@ export class StocksComponent implements OnInit {
 
   constructor(private _http: HttpService, private router: Router) { }
 
+  stockIndex = []
   ngOnInit(): void {
     this.getstocks();
   }
 
   getstocks(): void {
     console.log("hello");
-    this._http.getstocks();
+    this._http.getstocks().subscribe(data => {
+      console.log(data);
+
+      for (let x in data) {
+        this.stockIndex.push(data[x])
+      }
+      //console.log(this.stockIndex)
+    })
   }
 
 }
